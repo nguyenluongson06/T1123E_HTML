7160efac65b119932be800a32c9c5c648524380f
nguyenluongson06 <nguyenluongson06@gmail.com>
2024-01-25 23:23:02 +0700

    added solution in slot 7 (no css, too lazy)

---
+++ slot7/main.js
@@ -0,0 +1,72 @@
+//generate 6 non-duplicated numbers in [a, b] range
+//return the resulting array
+function generate(){
+    var arr = [];
+    for (var i = 0; i <= 45; i++){
+        arr[i] = i;
+    }
+
+    var result = [];
+    for (var i = 0; i < 6; i++){
+        var index = Math.floor(Math.random() * 46);
+        result.push(arr[index]);
+        arr.pop(arr[index]);
+    }
+    return(result);
+}
+
+//compare 2 provided array & return the result based on how many duplicates exist b/w 2 arrays
+function compare(input, result){
+    var duplicate = 0;
+    input.forEach(element => {
+        if (result.includes(element)) {
+            duplicate++;
+        }
+    });
+
+    return display_result(duplicate);
+}
+
+//display result correspondent to the number of duplicates
+function display_result(duplicate){
+    if (duplicate >= 5){
+        return "Bạn đã trúng Jackpot 100.000.000.000VND!"
+    } else if (duplicate == 4){
+        return "Bạn đã trúng 10.000.000VND!"
+    } else if (duplicate == 3){
+        return "Bạn đã trúng 100.000VND!"
+    } else {
+        return "Bạn không trúng thưởng, chúc may mắn lần sau."
+    }
+}
+
+//get the submit button and attach event listener to it
+var button = document.getElementById("btn");
+console.log(button);
+button.addEventListener("click", function() {
+
+    //get all input items to a HTMLCollection
+    var input_list = document.getElementsByClassName("input");
+    var input_value = [];
+    //get value of each input then push into a list
+    for (let element of input_list) {
+        var number = parseInt(element.value);
+        input_value.push(number);
+    }
+
+    //generate the result array
+    var output_value = generate();
+    //get all input.output items to a HTMLCollection
+    var output_list = document.getElementsByClassName("output");
+    var count = 0;
+    //change value of each output item
+    for (let element of output_list) {
+        element.value = output_value[count];
+        count++;
+    }
+    
+    var result_element = document.createElement("p");
+    result_element.innerHTML = compare(input_value, output_value);
+    var result_container = document.getElementById("result-container");
+    result_container.appendChild(result_element);
+})
